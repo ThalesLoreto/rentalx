6a9d5a30cf3280bd29e7ccc9b44d7b7502265742
ThalesLoreto <tloreto.dev@gmail.com>
2022-02-11 17:58:31 -0300

    delete username column in table

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -14,13 +14,11 @@ class UsersRepository implements IUsersRepository {
   async create({
     name,
     email,
-    username,
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
-      username,
       email,
       password,
       driver_license,
