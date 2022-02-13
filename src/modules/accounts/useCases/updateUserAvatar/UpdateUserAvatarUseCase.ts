a65f0fb1099703859df583c3f6455fda22ad43a8
ThalesLoreto <tloreto.dev@gmail.com>
2022-02-13 01:48:19 -0300

    add DeleteFile method to autodelete files

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import { AppError } from '../../../../errors/AppError';
+import { deleteFile } from '../../../../utils/file';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -23,6 +24,10 @@ class UpdateUserAvatarUseCase {
     const user = await this.usersRepository.findById(user_id);
     if (!user) {
       throw new AppError('User not exists');
+    }
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
     }
 
     user.avatar = avatar_file;
