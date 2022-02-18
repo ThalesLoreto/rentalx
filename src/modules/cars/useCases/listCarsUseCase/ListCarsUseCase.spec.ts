523c28130487cbad107981e4ed24bc6452b0d5c6
ThalesLoreto <tloreto.dev@gmail.com>
2022-02-18 18:11:28 -0300

    finish to cover all tests by list all available cars with filters

--- src/modules/cars/useCases/listCarsUseCase/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCarsUseCase/ListCarsUseCase.spec.ts
@@ -55,11 +55,73 @@ describe('List cars', () => {
       brand: 'Brand_Filter',
     });
 
-    console.log(cars);
-
     expect(cars).toEqual(
       expect.arrayContaining([
         expect.objectContaining({ brand: 'Brand_Filter' }),
+      ]),
+    );
+  });
+
+  it('should be able to list all available cars filtering by name', async () => {
+    await createCarUseCase.execute({
+      name: 'Car Searched',
+      description: 'Test Description Car',
+      daily_rate: 160,
+      fine_amount: 30,
+      brand: 'AnyBrand',
+      category_id: 'category_uuid',
+      license_plate: 'OWK-2351',
+    });
+
+    await createCarUseCase.execute({
+      name: 'Incorrect Car',
+      description: 'Test Description Car',
+      daily_rate: 160,
+      fine_amount: 30,
+      category_id: 'category_uuid',
+      brand: 'AnyBrand2',
+      license_plate: 'FHK-8365',
+    });
+
+    const cars = await listCarsUseCase.execute({
+      name: 'Car Searched',
+    });
+
+    expect(cars).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ name: 'Car Searched' }),
+      ]),
+    );
+  });
+
+  it('should be able to list all available cars filtering by category', async () => {
+    await createCarUseCase.execute({
+      category_id: 'correct-category-uuid',
+      name: 'Car Correct',
+      description: 'Test Description Car',
+      daily_rate: 160,
+      fine_amount: 30,
+      brand: 'AnyBrand',
+      license_plate: 'PRJ-2394',
+    });
+
+    await createCarUseCase.execute({
+      category_id: 'invalid-uuid',
+      name: 'Incorrect Car',
+      description: 'Test Description Car',
+      daily_rate: 160,
+      fine_amount: 30,
+      brand: 'AnyBrand2',
+      license_plate: 'WJF-7392',
+    });
+
+    const cars = await listCarsUseCase.execute({
+      category_id: 'correct-category-uuid',
+    });
+
+    expect(cars).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ category_id: 'correct-category-uuid' }),
       ]),
     );
   });
