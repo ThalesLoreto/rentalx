6478a1edcc9668f52e47e8a3d03b27a1f250d30c
ThalesLoreto <tloreto.dev@gmail.com>
2022-03-15 21:14:30 -0300

    add supertest controller in category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,73 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { hash } from 'bcryptjs';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
+
+describe('Create Category Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const pass = await hash('admin', 8);
+
+    connection.query(
+      `INSERT INTO users(id, name, email, password, is_admin, driver_license, created_at)
+        VALUES('${id}', 'admin', 'admin@email.com', '${pass}', true, 'AB', 'now()')
+      `,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create a new category', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@email.com',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category SuperTest',
+        description: 'Description from test category',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('should not be able to recreate already exist category', async () => {
+    const responseToken = await request(app).post('/sessions').send({
+      email: 'admin@email.com',
+      password: 'admin',
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post('/categories')
+      .send({
+        name: 'Category SuperTest',
+        description: 'Description from test category',
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,14 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host?: string): Promise<Connection> => {
+export default async (host = 'db_postgres'): Promise<Connection> => {
   const connectionOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(connectionOptions, {
-      host: host ?? 'db_postgres',
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'dev_rentalx'
+          : connectionOptions.database,
     }),
   );
 };
